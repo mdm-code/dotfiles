0bbdb0d7fb9728789f559f37b090ef96b0dbd52b
Michał Adamczyk <michal.adamczyk.1990@gmail.com>
2019-12-14 20:36:01 +0100

    [Feature] Add a script for populating dotfiles
    Create symlinks to most crucial dotfiles.

---
+++ .dotfiles_setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+dir="${HOME}/.dotfiles"
+
+# Bash
+ln -sfv "${dir}/.bash_profile" "${HOME}"
+ln -sfv "${dir}/.bash_aliases" "${HOME}"
+ln -sfv "${dir}/.bashrc" "${HOME}"
+
+# Vim
+ln -sfv "${dir}/init.vim" "${HOME}/.vimrc"
+
+# Window management
+ln -sfv "${dir}/.skhdrc" "${HOME}"
+ln -sfv "${dir}/.tmux.conf" "${HOME}"
+ln -sfv "${dir}/.yabairc" "${HOME}"
+
+# Git completion
+ln -sfv "${dir}/.git-completion.sh" "${HOME}"
+
+# Dirs
+ln -sfv "${dir}/.mpd" "${HOME}"
+ln -sfv "${dir}/.ncmpcpp" "${HOME}"
+ln -sfv "${dir}/.iterm2" "${HOME}"
+ln -sfv "${dir}/.config/karabiner" "${HOME}/.config"
+
+# Scripts dir
+ln -sfv "${dir}/.scripts" "${HOME}"
