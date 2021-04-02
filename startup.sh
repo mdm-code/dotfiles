611954b66d5c3f585f0adb3226648fe44992b0d8
Michał Adamczyk <michal.adamczyk.1990@gmail.com>
2021-04-02 08:54:36 +0200

    [Feature] Startup script for MACOS
    This script kickstarts yabai, spacebar, skhd
    and karabiner on startup.
    Read the comments in startup.sh to get the
    idea of what's going on.

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# This is the startup script for MACOS
+# The file `com.startup.plist` is copied to /Library/LaunchAgents
+# and loaded permanently using `sudo launchctl load -w /Library/LaunchAgents/com.startup.plist`.
+# To stop the service from running on startup, replace `load` with `unload` in the command above.
+
+brew services start spacebar
+brew services start yabai
+brew services start skhd
+sudo launchctl load "/Library/LaunchDaemons/org.pqrs.karabiner.karabiner_grabber.plist" 2>/dev/null
+sudo launchctl load "/Library/LaunchDaemons/org.pqrs.karabiner.karabiner_observer.plist" 2>/dev/null
+launchctl enable gui/"$UID"/org.pqrs.karabiner.karabiner_console_user_server
+launchctl bootstrap gui/"$UID" /Library/LaunchAgents/org.pqrs.karabiner.karabiner_console_user_server.plist 2>/dev/null
+launchctl enable gui/"$UID"/org.pqrs.karabiner.karabiner_console_user_server
