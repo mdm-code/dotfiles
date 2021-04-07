ecd33e0ba1bc15e636afc5b0015473bf9a1dde29
Michał Adamczyk <michal.adamczyk.1990@gmail.com>
2021-04-07 15:18:36 +0200

    [Fix] Add load-sa on each startup
    Else it won't let you swap panes as
    it usually does.

--- startup.sh
+++ startup.sh
@@ -5,9 +5,9 @@
 # and loaded permanently using `sudo launchctl load -w /Library/LaunchAgents/com.startup.plist`.
 # To stop the service from running on startup, replace `load` with `unload` in the command above.
 
-sudo yabai --load-sa
 brew services start spacebar
 brew services start yabai
+sudo yabai --load-sa
 brew services start skhd
 sudo launchctl load "/Library/LaunchDaemons/org.pqrs.karabiner.karabiner_grabber.plist" 2>/dev/null
 sudo launchctl load "/Library/LaunchDaemons/org.pqrs.karabiner.karabiner_observer.plist" 2>/dev/null
