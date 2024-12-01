f91cb7c50ed588a53468e37430ed7daf4bf3695e
mdm-code <code.madamczyk@gmail.com>
2024-12-01 16:51:54 +0100

    Removed todos leftovers from neovim files

--- ansible/roles/neovim/files/init.lua
+++ ansible/roles/neovim/files/init.lua
@@ -2,8 +2,3 @@ require("settings")
 require("keymaps")
 require("autocmds")
 require("lazy_init")
-
--- TODO:
--- Add gofumpt installation to Go role in Ansible.
--- Add goimports installation to Go role in Ansible.
--- Add the lua-based setup to the Neovim role in Ansible.
